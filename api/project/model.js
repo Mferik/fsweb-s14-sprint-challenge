ea95eb5ca82101ee2174206b90ec19f21189721b
Mferik <mfatiherikk@gmail.com>
2023-04-29 14:37:18 +0300

    tasks will update

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,32 @@
 // `Proje` modeli buraya
+const db = require("../../data/dbConfig");
+
+const getAllProjects = async () => {
+  let allProjects = await db("projects");
+
+  let transformedProjects = allProjects.map((row) => {
+    return {
+      ...row,
+      project_completed: row.project_completed == 1,
+    };
+  });
+  return transformedProjects;
+};
+
+const getById = async (project_id) => {
+  let projects = await db("projects").where("project_id", project_id);
+  let project = projects[0];
+  project["project_completed"] = project.project_completed === 1;
+  return project;
+};
+
+const createProjects = async (project) => {
+  let [project_id] = await db("projects").insert(project);
+  return getById(project_id);
+};
+
+module.exports = {
+  getAllProjects,
+  createProjects,
+  getById,
+};
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,31 @@
 // bu`Task` modeli buraya
+const db = require("../../data/dbConfig");
+
+const getAllTasks = async () => {
+  let allTasks = await db("tasks as t")
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .select("t.*", "p.project_name", "p.project_description");
+
+  let transformedTasks = allTasks.map((item) => {
+    delete item.project_id;
+    return {
+      ...item,
+      task_completed: item.task_completed === 1,
+    };
+  });
+  return transformedTasks;
+};
+
+createTask = async (model) => {
+  const insertedTaskId = await db("tasks").insert(model);
+  const insertedRecord = await db("tasks")
+    .where("task_id", insertedTaskId)
+    .first();
+  insertedRecord["task_completed"] = insertedRecord.task_completed === 1;
+  return insertedRecord;
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+};
